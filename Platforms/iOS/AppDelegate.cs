0e4bf8eb1650f57d60df23329d53f106e2e75f5e
Maurice Ruffin <Tysons3x3@gmail.com>
2026-09-06 20:36:36 -0400

    Fix MAUI project folder structure

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace SDC_440;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
