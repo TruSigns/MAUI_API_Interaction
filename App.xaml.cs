4e456e31784475748066f842968e2962fe27c26b
Maurice Ruffin <Tysons3x3@gmail.com>
2026-09-06 20:11:29 -0400

    Complete MAUI UI controls

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MAUI_First_App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MAUI_First_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDisplayClicked(object sender, EventArgs e)
+        {
+            lblOutput.Text = txtInput.Text;
+        }
+
+        private void OnResetClicked(object sender, EventArgs e)
+        {
+            txtInput.Text = string.Empty;
+            lblOutput.Text = string.Empty;
+        }
+    }
+}
